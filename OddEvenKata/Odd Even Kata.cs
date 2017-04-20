4c3693d4fbf6a037aa68e6eae527a73bbe201965
alsoraimi <alsoraimi46@gmail.com>
2017-04-20 16:17:32 -0400

    initial commit pair programming

---
+++ OddEvenKata/Odd Even Kata.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddEvenKata
+{
+    class Odd_Even_Kata
+    {
+        public static string GetOddEvenKata(int x)
+        {
+
+            if (x % 2 == 0)
+                return "even";
+            if (x == 1 || x == 3)
+                return "odd";
+            
+            for (int a = 2; a <= x / 2; a++)
+            {
+                if (x % a == 0)
+                {
+                    
+                    return "odd";
+
+                }
+               
+            }
+            return "prime";
+
+        }
+
+    }
+}
+
+    
+
+
---
+++ OddEvenKata/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OddEvenKata
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string Expected ="odd";
+            Assert.AreEqual(Expected, Odd_Even_Kata.GetOddEvenKata(1));
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string Expected = "even";
+            Assert.AreEqual(Expected, Odd_Even_Kata.GetOddEvenKata(2));
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string Expected = "odd";
+            Assert.AreEqual(Expected, Odd_Even_Kata.GetOddEvenKata(3));
+
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string Expected = "even";
+            Assert.AreEqual(Expected, Odd_Even_Kata.GetOddEvenKata(4));
+
+        }
+        [TestMethod]
+        public void TestMethod53()
+        {
+            string Expected = "prime";
+            Assert.AreEqual(Expected, Odd_Even_Kata.GetOddEvenKata(53));
+
+        }
+
+        [TestMethod]
+        public void TestMethod51()
+        {
+            string Expected = "odd";
+            Assert.AreEqual(Expected, Odd_Even_Kata.GetOddEvenKata(51));
+
+        }
+
+
+
+    }
+}
